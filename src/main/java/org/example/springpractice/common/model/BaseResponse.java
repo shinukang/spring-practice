1cb15e340d78f9ae273f748f0527fa068bbf3a04
shinukang <shinu9643@gmail.com>
2026-02-26 15:35:38 +0900

    [feat] 공용 응답 클래스 수정 - 반환 타입 ResponseEntity -> BaseResponse로 수정

--- src/main/java/org/example/springpractice/common/model/BaseResponse.java
+++ src/main/java/org/example/springpractice/common/model/BaseResponse.java
@@ -1,7 +1,6 @@
 package org.example.springpractice.common.model;
 
 import lombok.Builder;
-import org.springframework.http.ResponseEntity;
 
 @Builder
 public class BaseResponse<T> {
@@ -9,21 +8,17 @@ public class BaseResponse<T> {
     private BaseResponseStatus status;
     private T result;
 
-    public static <T> ResponseEntity success(T result) {
-        return ResponseEntity.ok(BaseResponse.builder()
+    public static <T> BaseResponse success(T result) {
+        return BaseResponse.builder()
                 .status(BaseResponseStatus.SUCCESS)
                 .result(result)
-                .build());
+                .build();
     }
 
-    public static <T> ResponseEntity failure(BaseResponseStatus status) {
-        return failure(null, status);
-    }
-
-    public static <T> ResponseEntity failure(T result, BaseResponseStatus status) {
-        return ResponseEntity.ok(BaseResponse.builder()
+    public static <T> BaseResponse failure(T result, BaseResponseStatus status) {
+        return BaseResponse.builder()
                 .status(status)
                 .result(result)
-                .build());
+                .build();
     }
 }

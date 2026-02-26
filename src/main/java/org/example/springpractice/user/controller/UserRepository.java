bdff2418b328024ca072426dadaa9c2f89fc709c
shinukang <shinu9643@gmail.com>
2026-02-26 16:50:07 +0900

    [feat] 회원 가입 기능 추가 - 회원 클래스 추가 - 컨트롤러, 서비스, 레포지토리 클래스 추가 - 중복 이메일 예외 추가

---
+++ src/main/java/org/example/springpractice/user/controller/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.springpractice.user.controller;
+
+import org.example.springpractice.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/org/example/springpractice/user/model/User.java
@@ -0,0 +1,22 @@
+package org.example.springpractice.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
